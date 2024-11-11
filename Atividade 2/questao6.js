36231d0ce9700923a51bdc6ce0d18f11e97af100
Luc4sborges <142754951+Luc4sborges@users.noreply.github.com>
2024-11-11 20:52:49 -0300

    Create questao6.js

---
+++ Atividade 2/questao6.js
@@ -0,0 +1,10 @@
+const nome = "Ely";
+const TempoDePagamento = 120.56;
+const Linguagem = "TypeScript";
+const mensagem = `
+${nome}
+My payment time is ${TempoDePagamento}
+and
+my preferred language is ${Linguagem}
+`;
+console.log(mensagem);
