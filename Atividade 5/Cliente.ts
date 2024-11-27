321204ed79a7dc6c5d765f4bf3bec21ffdbb53a4
Luc4sborges <142754951+Luc4sborges@users.noreply.github.com>
2024-11-27 19:59:44 -0300

    Update Cliente.ts

--- Atividade 5/Cliente.ts
+++ Atividade 5/Cliente.ts
@@ -1,11 +1,12 @@
-class Cliente {
+import { Conta } from './Conta';
+export class Cliente {
     idCliente: number;
     nomeCliente: string;
     cpfCliente: string;
     dtNasCliente: Date;
     contasCliente: Conta[];
 
-    constructor(idCliente: number, nomeCliente: string, cpfCliente: string, dtNasCliente: Date){
+    constructor(idCliente: number, nomeCliente: string, cpfCliente: string, dtNasCliente: Date) {
         this.idCliente = idCliente;
         this.nomeCliente = nomeCliente;
         this.cpfCliente = cpfCliente;
@@ -13,6 +14,7 @@ class Cliente {
         this.contasCliente = [];
     }
 
-    adicionarConta(conta: Conta): void { 
-        this.contasCliente.push(conta); }
+    adicionarConta(conta: Conta): void {
+        this.contasCliente.push(conta);
+    }
 }
