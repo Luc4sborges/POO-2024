61ddb924ad9226961f1abe3d75efc8c21e4948f4
Luc4sborges <142754951+Luc4sborges@users.noreply.github.com>
2024-11-27 18:33:01 -0300

    Create Cliente.ts

---
+++ Atividade 5/Cliente.ts
@@ -0,0 +1,18 @@
+class Cliente {
+    idCliente: number;
+    nomeCliente: string;
+    cpfCliente: string;
+    dtNasCliente: Date;
+    contasCliente: Conta[];
+
+    constructor(idCliente: number, nomeCliente: string, cpfCliente: string, dtNasCliente: Date){
+        this.idCliente = idCliente;
+        this.nomeCliente = nomeCliente;
+        this.cpfCliente = cpfCliente;
+        this.dtNasCliente = dtNasCliente;
+        this.contasCliente = [];
+    }
+
+    adicionarConta(conta: Conta): void { 
+        this.contasCliente.push(conta); }
+}
