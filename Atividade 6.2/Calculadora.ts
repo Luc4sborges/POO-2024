5ca5e9a74a49c8c48547e6c896413225a32fc3a2
Luc4sborges <142754951+Luc4sborges@users.noreply.github.com>
2024-12-22 08:15:46 -0300

    Create Calculadora.ts

---
+++ Atividade 6.2/Calculadora.ts
@@ -0,0 +1,25 @@
+class Calculadora {
+    private operando1: number;
+    private operando2: number;
+
+    constructor(operando1: number, operando2: number) {
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+    }
+
+    public somar(): number {
+        return this.operando1 + this.operando2;
+    }
+
+    public subtrair(): number {
+        return this.operando1 - this.operando2;
+    }
+}
+
+const calculadora = new Calculadora(10, 5);
+console.log(calculadora.somar()); 
+console.log(calculadora.subtrair());
+
+
+console.log(calculadora.operando1); // Erro: Property 'operando1' is private and only accessible within class 'Calculadora'.
+console.log(calculadora.operando2); // Erro: Property 'operando2' is private and only accessible within class 'Calculadora'.
