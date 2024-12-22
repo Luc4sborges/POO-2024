b00fb95d20893e5701f0fb4106a0fd9c68a1141e
Luc4sborges <142754951+Luc4sborges@users.noreply.github.com>
2024-12-22 08:41:39 -0300

    Update Banco.ts

--- Atividade 6.2/Banco.ts
+++ Atividade 6.2/Banco.ts
@@ -9,54 +9,55 @@ export class Banco {
 
     // Métodos Públicos
     inserirConta(conta: Conta): void {
-        for (let con of this.contas) {
-            if (con.idConta === conta.idConta) {
-                console.log("Erro: Já existe uma conta com este ID.");
-                return;
-            }
-            if (con.numero === conta.numero) {
-                console.log("Erro: Já existe uma conta com este número.");
-                return;
-            }
+    for (let con of this.contas) {
+        if (con.getIdConta() === conta.getIdConta()) {
+            console.log("Erro: Já existe uma conta com este ID.");
+            return;
         }
-        this.contas.push(conta);
-        console.log("Conta inserida com sucesso.");
+        if (con.getNumero() === conta.getNumero()) {
+            console.log("Erro: Já existe uma conta com este número.");
+            return;
+        }
     }
+    this.contas.push(conta);
+    console.log("Conta inserida com sucesso.");
+}
 
     inserirCliente(cliente: Cliente): void {
         for (let cli of this.clientes) {
-            if (cli.idCliente === cliente.idCliente) {
+            if (cli.getIdCliente() === cliente.getIdCliente()) {
                 console.log("Erro: Já existe um cliente com este ID.");
                 return;
             }
-            if (cli.cpfCliente === cliente.cpfCliente) {
+            if (cli.getCpfCliente() === cliente.getCpfCliente()) {
                 console.log("Erro: Já existe um cliente com este CPF.");
                 return;
             }
         }
         this.clientes.push(cliente);
         console.log("Cliente inserido com sucesso.");
-    }
+}
 
-    consultarConta(numero: string): Conta | void {
-        let contaProcurada: Conta | undefined;
-        for (let conta of this.contas) {
-            if (conta.numero === numero) {
-                contaProcurada = conta;
-                break;
-            }
-        }
-        if (contaProcurada) {
-            return contaProcurada;
-        } else {
-            console.log("Conta não encontrada.");
+
+   consultarConta(numero: string): Conta | void {
+    let contaProcurada: Conta | undefined;
+    for (let conta of this.contas) {
+        if (conta.getNumero() === numero) {
+            contaProcurada = conta;
+            break;
         }
     }
+    if (contaProcurada) {
+        return contaProcurada;
+    } else {
+        console.log("Conta não encontrada.");
+    }
+}
 
     consultarCliente(cpfdoCliente: string): Cliente | void {
         let clienteProcurado: Cliente | undefined;
         for (let cliente of this.clientes) {
-            if (cliente.cpfCliente === cpfdoCliente) {
+            if (cliente.getCpfCliente() === cpfdoCliente) {
                 clienteProcurado = cliente;
                 break;
             }
@@ -66,15 +67,15 @@ export class Banco {
         } else {
             console.log("Cliente não encontrado.");
         }
-    }
+}
 
     associarContaCliente(numeroConta: string, cpfCliente: string): void {
         let conta = this.consultarConta(numeroConta);
         let cliente = this.consultarCliente(cpfCliente);
         if (conta && cliente) {
             let contaJaAssociada = false;
-            for (let c of cliente.contasCliente) {
-                if (c.numero === numeroConta) {
+            for (let c of cliente.getContasCliente()) {
+                if (c.getNumero() === numeroConta) {
                     contaJaAssociada = true;
                     break;
                 }
@@ -82,16 +83,16 @@ export class Banco {
             if (contaJaAssociada) {
                 console.log("A conta já está associada a este cliente.");
             } else {
-                cliente.adicionarConta(conta);
-                console.log("Conta associada ao cliente com sucesso.");
-            }
+            cliente.adicionarConta(conta);
+            console.log("Conta associada ao cliente com sucesso.");
         }
     }
+}
 
     listarContasCliente(cpf: string): Conta[] {
         let cliente = this.consultarCliente(cpf);
         if (cliente) {
-            return cliente.contasCliente;
+            return cliente.getContasCliente();
         } else {
             console.log("Cliente não encontrado.");
             return [];
@@ -100,17 +101,19 @@ export class Banco {
 
     totalizarSaldoCliente(cpf: string): number {
         let cliente = this.consultarCliente(cpf);
-        let somaTotal = 0;
+            let somaTotal = 0;
         if (cliente) {
-            for (let c of cliente.contasCliente) {
-                somaTotal += c.saldo;
+            for (let c of cliente.getContasCliente()) {
+                somaTotal += c.getSaldo();
             }
             return somaTotal;
         } else {
             console.log("Cliente não encontrado.");
             return 0;
-        }
     }
+}
+
+   
 
     excluir(numero: string): void {
         let indiceProcurado: number = this.consultarPorIndice(numero); 
@@ -171,73 +174,74 @@ export class Banco {
         }
     }
 
+    transferir(numeroCredito: string, numeroDebito: string, valor: number): void {
+    let contaDebito = this.consultarConta(numeroDebito);
+    let contaCredito = this.consultarConta(numeroCredito);
+    if (contaDebito && contaCredito) {
+        if (contaDebito.getSaldo() >= valor) {
+            contaDebito.sacar(valor);
+            contaCredito.depositar(valor);
+            console.log(`Transferência de R$${valor} realizada com sucesso.`);
+        } else {
+            console.log("Erro: Saldo insuficiente na conta de débito.");
+        }
+    } else {
+        if (!contaDebito) {
+            console.log("Erro: Conta de débito não encontrada.");
+        }
+        if (!contaCredito) {
+            console.log("Erro: Conta de crédito não encontrada.");
+        }
+    }
+}
+
     sacar(numero: string, valor: number): void {
         let conta = this.consultarConta(numero);
         if (conta) {
-            if (conta.saldo >= valor) {
-                conta.saldo -= valor;
+            if (conta.getSaldo() >= valor) {
+                conta.sacar(valor);
                 console.log(`Saque de R$${valor} realizado com sucesso.`);
             } else {
                 console.log("Erro: Saldo insuficiente.");
             }
         } else {
             console.log("Erro: Conta não encontrada.");
-        }
-    }
+     }
+}
 
     depositar(numero: string, valor: number): void {
         let conta = this.consultarConta(numero);
         if (conta) {
-            conta.saldo += valor;
+            conta.depositar(valor);
             console.log(`Depósito de R$${valor} realizado com sucesso.`);
         } else {
             console.log("Conta não encontrada.");
-        }
     }
+}
 
-    transferir(numeroCredito: string, numeroDebito: string, valor: number): void {
-        let contaDebito: Conta | void = this.consultarConta(numeroDebito);
-        let contaCredito: Conta | void = this.consultarConta(numeroCredito);
-        if (contaDebito && contaCredito) {
-            if (contaDebito.saldo >= valor) {
-                this.sacar(numeroDebito, valor);
-                contaCredito.saldo += valor;
-                console.log(`Transferência de R$${valor} realizada com sucesso.`);
-            } else {
-                console.log("Erro: Saldo insuficiente na conta de débito.");
-            }
-        } else {
-            if (!contaDebito) {
-                console.log("Erro: Conta de débito não encontrada.");
-            }
-            if (!contaCredito) {
-                console.log("Erro: Conta de crédito não encontrada.");
-            }
-        }
-    }
 
     transferirParaVariasContas(numeroDebito: string, numerosCredito: string[], valor: number): void {
-        let contaDebito = this.consultarConta(numeroDebito);
-        if (contaDebito) {
-            let valorTotal = valor * numerosCredito.length;
-            if (contaDebito.saldo >= valorTotal) {
-                for (let numeroCredito of numerosCredito) {
-                    let contaCredito = this.consultarConta(numeroCredito);
-                    if (contaCredito) {
-                        this.sacar(numeroDebito, valor);
-                        contaCredito.depositar(valor);
-                        console.log(`Transferência de R$${valor} para conta ${numeroCredito} realizada com sucesso.`);
-                    } else {
-                        console.log(`Erro: Conta de crédito ${numeroCredito} não encontrada.`);
-                    }
+    let contaDebito = this.consultarConta(numeroDebito);
+    if (contaDebito) {
+        let valorTotal = valor * numerosCredito.length;
+        if (contaDebito.getSaldo() >= valorTotal) {
+            for (let numeroCredito of numerosCredito) {
+                let contaCredito = this.consultarConta(numeroCredito);
+                if (contaCredito) {
+                    contaDebito.sacar(valor);
+                    contaCredito.depositar(valor);
+                    console.log(`Transferência de R$${valor} para conta ${numeroCredito} realizada com sucesso.`);
+                } else {
+                    console.log(`Erro: Conta de crédito ${numeroCredito} não encontrada.`);
                 }
-            } else {
-                console.log("Erro: Saldo insuficiente na conta de débito para todas as transferências.");
             }
         } else {
-            console.log("Erro: Conta de débito não encontrada.");
+            console.log("Erro: Saldo insuficiente na conta de débito para todas as transferências.");
         }
+    } else {
+        console.log("Erro: Conta de débito não encontrada.");
     }
+}
 
     quantidadeDeContas(): number {
         return this.contas.length;
